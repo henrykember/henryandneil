66ad4f682dd00827ee5040f809a99c7cfa32dc4b
henrykember <hmkember@gmail.com>
2018-08-06 09:07:45 -0400

    thumbnails and links
    Adding thumbnails and links, but can't  get video to play in modal

--- hn-menu.js
+++ hn-menu.js
@@ -7,19 +7,43 @@ $(document).ready(function() {
 $("#gdinttitle").on("click", function(){
   $(".gdinterviews, .vidcontainer, .vidwrapper").addClass("active1"); 
 });
+$("#volvoembracetitle").on("click", function(){
+  $(".volvoembrace, .vidcontainer, .vidwrapper").addClass("active1"); 
+});
+$("#cginftitle").on("click", function(){
+  $(".cginfinite, .vidcontainer, .vidwrapper").addClass("active1"); 
+});
+$("#cginftitle").on("click", function(){
+  $(".volvoembrace, .vidcontainer, .vidwrapper").addClass("active1"); 
+});
+
 
 //removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
 $(".close").on("click", function(){
-  $(".gdinterviews").removeClass("active1");
+  $(".gdinterviews, .volvoembrace, .cginfinite").removeClass("active1");
 });
 
-$("#gdinttitle").on("click", function(){
+$("#gdinttitle, #volvoembracetitle, #cginftitle").on("click", function(){
   $("body").addClass("pause"); 
 });
 
 $(".close").on("click", function(){
   $("body").removeClass("pause");
 });
+
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+// $(".close").on("click", function(){
+//   $(".volvoembrace").removeClass("active1");
+// });
+
+// $("#volvoembracetitle").on("click", function(){
+//   $("body").addClass("pause"); 
+// });
+
+// $(".close").on("click", function(){
+//   $("body").removeClass("pause");
+// });
 
 // $(".open").on("click", function(){
 //   $(".popup-overlay, .popup-content").addClass("active2");
